c5c571d482b2eed700b7ebca04066910ace24af1
AlixGith <139876103+AlixGith@users.noreply.github.com>
2026-02-21 17:11:35 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ("single_episode.html", include_str!("templates/single_episode.html")),
         ("single_serie.html", include_str!("templates/single_serie.html")),
         ("last_series.html", include_str!("templates/last_series.html")),
-
+		("last_episodes.html", include_str!("templates/last_episodes.html")),
         ("style.css", include_str!("templates/style.css")),
 
         ("img/download.svg", include_str!("templates/img/download.svg")),
