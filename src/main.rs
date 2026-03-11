e8722d896869321ab4d4fde29071eddd52e94f12
AlixGith <139876103+AlixGith@users.noreply.github.com>
2026-03-11 15:54:37 +0100

    add wages

--- src/main.rs
+++ src/main.rs
@@ -137,7 +137,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ("Boulangerie_ile.html", include_str!("templates/Boulangerie_ile.html")),
 
         ("carte.html", include_str!("templates/carte.html")),
-        
+                
+        ("Animation_connexion.html", include_str!("templates/Animation_connexion.html")),
         ("Animation_reseau.html", include_str!("templates/Animation_reseau.html")),
         ("style.css", include_str!("templates/style.css")),
 
@@ -193,6 +194,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     	let _ = std::fs::write(
         oma.args.output.clone() + "/Animation_reseau.html",
         tera.render("Animation_reseau.html", &context).unwrap()
+    );
+    let _ = std::fs::write(
+        oma.args.output.clone() + "/Animation_connexion.html",
+        tera.render("Animation_connexion.html", &context).unwrap()
     );
     let _ = std::fs::write(
         oma.args.output.clone() + "/articles/Critique_du_Parti.html",
